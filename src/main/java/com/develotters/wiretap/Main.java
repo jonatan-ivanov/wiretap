c7c610e6e916304ee21aa7b220134de28d5d66f0
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2021-12-10 23:56:00 -0800

    Add http server

--- src/main/java/com/develotters/wiretap/Main.java
+++ src/main/java/com/develotters/wiretap/Main.java
@@ -3,7 +3,6 @@ package com.develotters.wiretap;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
@@ -12,28 +11,31 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
 import reactor.netty.Connection;
+import reactor.netty.DisposableServer;
 import reactor.netty.NettyInbound;
 import reactor.netty.NettyOutbound;
+import reactor.netty.http.server.HttpServer;
+import reactor.netty.http.server.HttpServerRoutes;
 import reactor.netty.tcp.TcpServer;
 
 public class Main {
 	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
 	public static void main(String[] args) {
-		TcpServer.create()
-				.port(getPort(args))
-				.wiretap(true)
-				.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 3_000)
-				.doOnBound(disposableServer -> LOGGER.info("Listening on " + disposableServer.port()))
-				.doOnConnection(Main::timeoutConfigurer)
-				.handle(Main::payloadHandler)
-				.bindNow()
-				.onDispose()
-				.block();
+		List<String> arguments = Arrays.stream(args).toList();
+		getServer(getServerType(arguments), getPort(arguments)).onDispose().block();
 	}
 
-	private static int getPort(String[] args) {
-		List<String> arguments = Arrays.stream(args).toList();
+	private static ServerType getServerType (List<String> arguments) {
+		if (arguments.contains("--http")) {
+			return ServerType.HTTP;
+		}
+		else {
+			return ServerType.TCP;
+		}
+	}
+
+	private static int getPort(List<String> arguments) {
 		int portIndex = arguments.indexOf("--port") + 1;
 		if (portIndex > 0) {
 			return Integer.parseInt(arguments.get(portIndex));
@@ -42,13 +44,52 @@ public class Main {
 		return 8080;
 	}
 
-	private static Publisher<Void> payloadHandler(NettyInbound in, NettyOutbound out) {
+	private static DisposableServer getServer(ServerType serverType, int port) {
+		return switch (serverType) {
+			case HTTP -> createHttpServer(port);
+			default -> createTcpServer(port);
+		};
+	}
+
+	private static DisposableServer createTcpServer(int port) {
+		return TcpServer.create()
+				.port(port)
+				.wiretap(true)
+				.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 3_000)
+				.doOnBound(Main::unBoundHandler)
+				.doOnConnection(Main::timeoutConfigurer)
+				.handle(Main::tcpPayloadHandler)
+				.bindNow();
+	}
+
+	private static DisposableServer createHttpServer(int port) {
+		return HttpServer.create()
+				.port(port)
+				.wiretap(true)
+				.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 3_000)
+				.doOnBound(Main::unBoundHandler)
+//				.doOnConnection(Main::timeoutConfigurer)
+				.route(Main::httpRouter)
+				.bindNow();
+	}
+
+	private static void unBoundHandler(DisposableServer server) {
+		LOGGER.info("Listening on " + server.port());
+	}
+
+	private static void timeoutConfigurer(Connection connection) {
+		connection.addHandler(new ReadTimeoutHandler(10, TimeUnit.SECONDS));
+	}
+
+	private static Publisher<Void> tcpPayloadHandler(NettyInbound in, NettyOutbound out) {
 		return in.receive()
 				.asString()
 				.flatMap(payload -> out.sendString(Mono.empty()));
 	}
 
-	private static void timeoutConfigurer(Connection connection) {
-		connection.addHandler(new ReadTimeoutHandler(5, TimeUnit.SECONDS));
+	private static void httpRouter(HttpServerRoutes routes) {
+		routes
+				.ws("/ws", (wsIn, wsOut) -> wsOut.sendString(wsIn.receive().asString().map(msg -> "echo: " + msg)))
+				.route(rq -> true, (rq, rs) -> rs.sendString(Mono.just("ok")));
 	}
 }
---
+++ src/main/java/com/develotters/wiretap/ServerType.java
@@ -0,0 +1,5 @@
+package com.develotters.wiretap;
+
+public enum ServerType {
+	TCP, HTTP
+}
